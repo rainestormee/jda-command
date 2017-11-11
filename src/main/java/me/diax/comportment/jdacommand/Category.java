7928b216f0341c8cd02b951305c365daededf46b
AdrianTodt <adriantodt@outlook.com>
2017-11-11 18:54:47 -0200

    Categories!

---
+++ src/main/java/me/diax/comportment/jdacommand/Category.java
@@ -0,0 +1,15 @@
+package me.diax.comportment.jdacommand;
+
+import java.lang.annotation.*;
+
+/**
+ * This represents a generic category.
+ * @author AdrianTodt
+ * @since 1.1.2
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.PACKAGE})
+public @interface Category {
+    String value();
+}
